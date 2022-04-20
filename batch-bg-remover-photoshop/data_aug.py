335f7bd0b0b8a7b00219dfe680a2784708fd52ec
ylab604 <99255566+ylab604@users.noreply.github.com>
2022-04-20 12:05:45 +0900

    Update data_aug.py

--- batch-bg-remover-photoshop/data_aug.py
+++ batch-bg-remover-photoshop/data_aug.py
@@ -48,3 +48,33 @@ for i in range(len(data)):
     out_orgin = im_new_orgin.rotate(angle)
     out_orgin.save(data_root+"\\orgin_rotate_"+filename)
 ######
+#again match imbalance
+######
+import os
+import glob
+data_root = "C:\\Users\\user\\Downloads\\Image_train_remove_aug"
+data_aug = os.listdir(data_root)
+
+for i in range(len(data_aug)):
+    try:
+        name_list = data_aug[i].split('_')
+        height = name_list[-2]
+        weight = name_list[-1][:-8]
+        BMI_ = (int(weight) / 100000) / ((int(height) / 100000) ** 2)
+        if BMI_ > 25 and BMI_ < 35 :
+            if "orgin" or "fliped" in name_list:
+                if os.path.exists(data_root):
+                    os.remove(os.path.join(data_root, data_aug[i]))
+
+    except:
+        data_aug[i]=data_aug[i].replace(']','_')
+        name_list = data_aug[i].split('_')
+        height = name_list[-3]
+        weight = name_list[-2]
+        BMI_ = (int(weight) / 100000) / ((int(height) / 100000) ** 2)
+        if BMI_ > 25 and BMI_ < 35 :
+            if "orgin" or "fliped" in name_list:
+                if os.path.exists(data_root):
+                    os.remove(os.path.join(data_root, data_aug[i]))
+
+#data_aug       
