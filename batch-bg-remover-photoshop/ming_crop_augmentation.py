ac3b77868328d08192b2f6153d9b20349efd21f2
mostar39 <58336937+mostar39@users.noreply.github.com>
2022-05-23 16:12:07 +0900

    Update ming_crop_augmentation.py

--- batch-bg-remover-photoshop/ming_crop_augmentation.py
+++ batch-bg-remover-photoshop/ming_crop_augmentation.py
@@ -31,32 +31,3 @@ for i in tqdm(range(len(all_image))) :
     cv2.imwrite(os.path.join(save_root, 'crop_'+all_image[i]), image_aug)
 
 
-
-#
-#
-#
-#
-#
-# test_image = cv2.imread('Image_train_remove_aug_again/0_F_15_162560_6123497.jpg.jpg')
-# h,w,c= test_image.shape
-# cv2.imshow('hi',test_image)
-# cv2.waitKey(0)
-#
-# cv2.imwrite('hihi.jpg', test_image)
-#
-#
-#
-# aug_1 = iaa.Sequential([
-#     iaa.PadToFixedSize(width=w*0.8, height=h*0.8),
-# ])
-#
-# aug_2 = iaa.Sequential([
-#     iaa.CropToFixedSize(width=w*0.8, height=h*0.8)
-# ])
-#
-# image_aug = aug(image=test_image)
-# cv2.imshow('hi',image_aug)
-# cv2.imwrite('check_color.jpg', image_aug)
-#
-# aug_image = aug.show_grid(test_image, cols=1, rows=1)
-# cv2.imshow('hi',aug_image, cols=1, rows=1)
